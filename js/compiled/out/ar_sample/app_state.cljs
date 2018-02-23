d9005fe8198932c75111d534c31f5e904cb2ede5
Your Name <you@example.com>
2018-02-23 19:14:54 +0700

    Updates

--- js/compiled/out/ar_sample/app_state.cljs
+++
@@ -1,5 +0,0 @@
-(ns ar-sample.app-state
-  (:require [integrant.core :as ig]))
-
-(defmethod ig/init-key :app-state [_ _]
-  (atom {}))
--- js/compiled/out/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [ar-sample.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/ar-sample.core.on-js-reload (apply js/ar-sample.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'ar-sample.core/on-js-reload' is missing"))), :open-urls ["http://localhost:3449/index.html"], :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
